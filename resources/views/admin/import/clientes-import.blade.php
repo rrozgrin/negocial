d3ef8bcf949f267e3cde571e507792b284940845
rozgrin <rrozgrin@gmail.com>
2023-07-11 10:07:59 -0300

    Incluído módulo administrativo

--- resources/views/admin/import/clientes-import.blade.php
+++ resources/views/admin/import/clientes-import.blade.php
@@ -1,23 +1,17 @@
-@extends('admin.layout')
-@section('content')
-
 <div class="card">
-    <div class="card-header py-3">
-        Incluir Clientes
+    <div class="card-header bg-primary m-2 py-3">
+        Incluir clientes
     </div>
     <div class="card-body">
         <form method="POST" action="import-clientes/" enctype="multipart/form-data">
             @csrf
             <div class="form-group">
                 <label for="name">Selecione o arquivo</label>
-                <input type="file" class="form-control" id="file" name="file">
+                <input type="file" class="form-control pb-5 pt-3" id="file" name="file">
             </div>
-
             <div class="form-group">
                 <button class="btn btn-success" value="file" type="submit">Carregar BD</button>
             </div>
         </form>
     </div>
 </div>
-
-@endsection
